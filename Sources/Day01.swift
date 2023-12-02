41bf3dfb1f4628a84bbadd22fa5a25d5115d26ba
Brent Tunnicliff <brenttunnicliff@gmail.com>
2023-12-02 13:04:55 +1100

    Day 1 part 2.

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -11,6 +11,18 @@ struct Day01: AdventDay {
         }
     }
 
+    private let namedDigits = [
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven": 7,
+        "eight": 8,
+        "nine": 9
+    ]
+
     func part1() -> Any {
         entities.reduce(into: 0) { partialResult, entity in
             let digits = entity.compactMap {
@@ -21,8 +33,50 @@ struct Day01: AdventDay {
             let last = digits.last ?? 0
             let newValue = Int("\(first)\(last)") ?? 0
 
+            partialResult += newValue
+        }
+    }
+
+    func part2() -> Any {
+        return entities.reduce(into: 0) { partialResult, entity in
+            let first = getDigit(entity, getKey: { $0.min() }) ?? 0
+            let last = getDigit(entity, getKey: { $0.max() }) ?? 0
+            let newValue = Int("\(first)\(last)") ?? 0
+
             partialResult = partialResult + newValue
         }
     }
 
+    private func getDigit(_ string: String, getKey: (([Int]) -> Int?)) -> Int? {
+        // Map the value to the index it starts on.
+        // Got solution from https://stackoverflow.com/a/58462061.
+        var values = namedDigits.reduce(into: [Int: Int]()) { partialResult, item in
+            let ranges = string.ranges(of: item.key)
+            guard !ranges.isEmpty else {
+                // No match
+                return
+            }
+
+            ranges.forEach { range in
+                let index = string.distance(from: string.startIndex, to: range.lowerBound)
+                partialResult[index] = item.value
+            }
+        }
+
+        // Now applying all digits to the same index:value map.
+        // This should not overwrite any values as namedDigit keys only contain letters, not digits.
+        string.enumerated().forEach {
+            guard let wholeNumberValue = $0.element.wholeNumberValue else {
+                return
+            }
+
+            values[$0.offset] = wholeNumberValue
+        }
+
+        guard let key = getKey(Array(values.keys)) else {
+            return nil
+        }
+
+        return values[key]
+    }
 }
--- Tests/Day01.swift
+++ Tests/Day01.swift
@@ -3,22 +3,38 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day01Tests: XCTestCase {
-    var challenge: Day01!
+    func testPart1() throws {
+        // given
+        let testData = """
+            1abc2
+            pqr3stu8vwx
+            a1b2c3d4e5f
+            treb7uchet
+            """
 
-    // Smoke test data provided in the challenge question
-    let testData = """
-        1abc2
-        pqr3stu8vwx
-        a1b2c3d4e5f
-        treb7uchet
-        """
+        // when
+        let result = Day01(data: testData).part1()
 
-    override func setUp() {
-        challenge = Day01(data: testData)
+        // then
+        XCTAssertEqual(String(describing: result), "142")
     }
 
-    func testPart1() throws {
-        let result = challenge.part1()
-        XCTAssertEqual(String(describing: result), "142")
+    func testPart2() throws {
+        // given
+        let testData = """
+            two1nine
+            eightwothree
+            abcone2threexyz
+            xtwone3four
+            4nineeightseven2
+            zoneight234
+            7pqrstsixteen
+            """
+
+        // when
+        let result = Day01(data: testData).part2()
+
+        // then
+        XCTAssertEqual(String(describing: result), "281")
     }
 }
