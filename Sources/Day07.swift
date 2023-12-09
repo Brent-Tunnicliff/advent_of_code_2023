f4131884f545d0326cdf39350b5f6958255219ab
Brent Tunnicliff <brenttunnicliff@gmail.com>
2023-12-09 21:27:07 +1100

    Day 7 part 1.

---
+++ Sources/Day07.swift
@@ -0,0 +1,149 @@
+// Copyright © 2023 Brent Tunnicliff <btunnicliff.dev@gmail.com>
+
+import Foundation
+
+struct Day07: AdventDay {
+    var data: String
+
+    func part1() -> Any {
+        getHands().sorted().reversed().enumerated().reduce(into: 0) { partialResult, hand in
+            partialResult += hand.element.bid * (hand.offset + 1)
+        }
+    }
+
+    func part2() -> Any {
+        return "Not implemented"
+    }
+
+    private func getHands() -> [Hand] {
+        data.split(separator: "\n")
+            .map {
+                $0.trimmingCharacters(in: .whitespaces).split(separator: " ")
+            }
+            .map { line in
+                precondition(line.count == 2, "Unexpected line count \(line)")
+
+                let bid = Int(line[1])!
+                let cards = line[0].map {
+                    Card(rawValue: $0.description)
+                }
+
+                return Hand(bid: bid, cards: cards)
+            }
+    }
+}
+
+/// Is of type `Int` to represent the card strength..
+private enum Card: Int, Comparable {
+    case ace = 0
+    case king
+    case queen
+    case jack
+    case ten
+    case nine
+    case eight
+    case seven
+    case six
+    case five
+    case four
+    case three
+    case two
+
+    init(rawValue: String) {
+        self =
+            switch rawValue {
+            case "A": .ace
+            case "K": .king
+            case "Q": .queen
+            case "J": .jack
+            case "T": .ten
+            case "9": .nine
+            case "8": .eight
+            case "7": .seven
+            case "6": .six
+            case "5": .five
+            case "4": .four
+            case "3": .three
+            case "2": .two
+            default:
+                preconditionFailure("Unexpected card \(rawValue)")
+            }
+    }
+
+    static func < (lhs: Card, rhs: Card) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+}
+
+/// Is of type `Int` to represent the hand strength.
+private enum HandType: Int, Comparable {
+    case fiveOfAKind = 0
+    case fourOfAKind
+    case fullHouse
+    case threeOfAKind
+    case twoPair
+    case onePair
+    case highCard
+
+    static func < (lhs: HandType, rhs: HandType) -> Bool {
+        lhs.rawValue < rhs.rawValue
+    }
+}
+
+private struct Hand: Comparable {
+    let bid: Int
+    let cards: [Card]
+    let type: HandType
+
+    init(bid: Int, cards: [Card]) {
+        precondition(cards.count == 5, "Unexpected cards count \(cards)")
+
+        self.bid = bid
+        self.cards = cards
+
+        let groupedCards = cards.reduce(into: [Card: Int]()) { partialResult, card in
+            let existingCount = partialResult[card] ?? 0
+            partialResult[card] = existingCount + 1
+        }.map { $0 }
+
+        type =
+            switch groupedCards.count {
+            case 1: .fiveOfAKind
+            case 2:
+                if groupedCards.first(where: { $0.value == 4 }) != nil {
+                    .fourOfAKind
+                } else {
+                    .fullHouse
+                }
+            case 3:
+                if groupedCards.first(where: { $0.value == 3 }) != nil {
+                    .threeOfAKind
+                } else {
+                    .twoPair
+                }
+            case 4: .onePair
+            case 5: .highCard
+            default:
+                preconditionFailure("Unexpected groupedCards \(groupedCards)")
+            }
+    }
+
+    static func < (lhs: Hand, rhs: Hand) -> Bool {
+        guard lhs.type == rhs.type else {
+            return lhs.type < rhs.type
+        }
+
+        for index in 0..<5 {
+            let lhsCard = lhs.cards[index]
+            let rhsCard = rhs.cards[index]
+
+            guard lhsCard != rhsCard else {
+                continue
+            }
+
+            return lhsCard < rhsCard
+        }
+
+        preconditionFailure("Unexpected end of loop comparing \(lhs.cards) and \(rhs.cards)")
+    }
+}
---
+++ Tests/Day07.swift
@@ -0,0 +1,31 @@
+// Copyright © 2023 Brent Tunnicliff <btunnicliff.dev@gmail.com>
+
+import XCTest
+
+@testable import AdventOfCode
+
+final class Day07Tests: XCTestCase {
+    private let testData = """
+        32T3K 765
+        T55J5 684
+        KK677 28
+        KTJJT 220
+        QQQJA 483
+        """
+
+    func testPart1() async throws {
+        // when
+        let result = Day07(data: testData).part1()
+
+        // then
+        XCTAssertEqual(String(describing: result), "6440")
+    }
+
+    func testPart2() async throws {
+        // when
+        let result = Day07(data: testData).part2()
+
+        // then
+        XCTAssertEqual(String(describing: result), "")
+    }
+}
